400234b779c4d04af1ddcd35763f226cba547bf3
drwiner <drwiner131@gmail.com>
2018-03-19 09:33:25 -0600

    heap + flaw file were missing

---
+++ Interfaces/IFlaw.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace BoltFreezer.Interfaces
+{
+    public interface IFlaw
+    {
+        string FlawType { get; set; }
+    }
+}
