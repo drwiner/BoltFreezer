3447bb3565819e33bd90aa63047b78491a0435f5
drwiner <drwiner131@gmail.com>
2018-03-26 20:23:40 -0600

    new DTN files

---
+++ DecompTools/Constraints.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BoltFreezer.Interfaces;
+using BoltFreezer.PlanTools;
+using BoltFreezer.Utilities;
+
+namespace BoltFreezer.DecompTools
+{
+    public class Linked : IConstraint
+    {
+        private string name;
+        private List<ITerm> terms;
+
+        public string Name {
+            get => name;
+            set { name = value; }
+        }
+
+        public List<ITerm> Terms
+        {
+            get { return terms; }
+            set { terms = value; }
+        }
+
+        public bool Check()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPlan Process(IPlan planToBuildOn)
+        {
+            return planToBuildOn;
+        }
+    }
+}
---
+++ DecompTools/Decomposition.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BoltFreezer.DecompTools
+{
+    public class Decomposition : IDecomposition
+    {
+        public List<ITask> SubTasks { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public List<IConstraint> Constraints { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
